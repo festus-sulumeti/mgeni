a3ea6fa96f7316cb59bab6c52a4b190b7ef358a4
festus-sulumeti <sulumetifestus@outlook.com>
2025-12-24 14:13:14 +0300

    feat: forgot password route path

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Hero from './components/HeroSecction/Hero'
 import SupportTeam from './components/SupportTeam'
 import LoginPage from './Auth/Login'
 import SignupPage from './Auth/Signup'
+import ForgotPassword from './Auth/ForgotPassword'
 
 function App() {
  
@@ -14,6 +15,7 @@ function App() {
         <Route path='/support' element={<SupportTeam />} />
         <Route path='/signin' element={<LoginPage />} />
         <Route path='/signup' element={<SignupPage />} />
+        <Route path='/forgot-password' element={<ForgotPassword />} />
       </Routes>
     </>
   )
